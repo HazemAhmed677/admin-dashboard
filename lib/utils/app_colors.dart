b25a25f3f92c68d4e6d162d6ea6f35ce75b56eb0
Hazem <hzm@gmail.com>
2024-07-23 00:39:20 +0300

    add app images & colors

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const primary = Color(0xff064061);
+  static const secondPrimary = Color(0xff4EB7F2);
+  static const white = Color(0xffFfFfFf);
+  static const semiWhite = Color(0xffFAFAFA);
+  static const grey = Color(0xffAAAAAA);
+}
