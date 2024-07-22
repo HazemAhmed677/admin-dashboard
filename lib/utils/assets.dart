09baf6f68f19f858a33ed645e3af4efee377de05
Hazem <hzm@gmail.com>
2024-07-23 00:48:29 +0300

    add assets class

---
+++ lib/utils/assets.dart
@@ -0,0 +1,63 @@
+class Assets {
+  Assets._();
+
+  /// Assets for imagesAvatar1
+  /// assets/images/avatar1.svg
+  static const String imagesAvatar1 = "assets/images/avatar1.svg";
+
+  /// Assets for imagesAvatar2
+  /// assets/images/avatar2.svg
+  static const String imagesAvatar2 = "assets/images/avatar2.svg";
+
+  /// Assets for imagesAvatar3
+  /// assets/images/avatar3.svg
+  static const String imagesAvatar3 = "assets/images/avatar3.svg";
+
+  /// Assets for imagesBalance
+  /// assets/images/balance.svg
+  static const String imagesBalance = "assets/images/balance.svg";
+
+  /// Assets for imagesCategory
+  /// assets/images/category.svg
+  static const String imagesCategory = "assets/images/category.svg";
+
+  /// Assets for imagesExpanses
+  /// assets/images/expanses.svg
+  static const String imagesExpanses = "assets/images/expanses.svg";
+
+  /// Assets for imagesGallery
+  /// assets/images/gallery.svg
+  static const String imagesGallery = "assets/images/gallery.svg";
+
+  /// Assets for imagesIncom
+  /// assets/images/incom.svg
+  static const String imagesIncom = "assets/images/incom.svg";
+
+  /// Assets for imagesInvestment
+  /// assets/images/investment.svg
+  static const String imagesInvestment = "assets/images/investment.svg";
+
+  /// Assets for imagesLogout
+  /// assets/images/logout.svg
+  static const String imagesLogout = "assets/images/logout.svg";
+
+  /// Assets for imagesMask
+  /// assets/images/mask.svg
+  static const String imagesMask = "assets/images/mask.svg";
+
+  /// Assets for imagesMy transaction
+  /// assets/images/my transaction.svg
+  static const String imagesmyTransaction = "assets/images/my transaction.svg";
+
+  /// Assets for imagesSetting
+  /// assets/images/setting.svg
+  static const String imagesSetting = "assets/images/setting.svg";
+
+  /// Assets for imagesStatistics
+  /// assets/images/statistics.svg
+  static const String imagesStatistics = "assets/images/statistics.svg";
+
+  /// Assets for imagesWallet
+  /// assets/images/wallet.svg
+  static const String imagesWallet = "assets/images/wallet.svg";
+}
